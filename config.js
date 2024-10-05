a2dcbcfd861cf0dde6737820dce73f84531d341a
aleperfect <143966062+aleperfect@users.noreply.github.com>
2024-10-04 19:02:43 -0500

    dfs

--- config.js
+++ config.js
@@ -11,50 +11,50 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['51992621601', 'Botcito Sexi', true],
-  ['51992621601', 'Botcito Sexi 1', true],
-  ['51992621601', 'Botcito Sexi 2', true],
-  ['51992621601', 'Botcito Sexi 3', true],
-  ['51992621601', 'Botcito Sexi 4', true],
-  ['51992621601', 'Botcito Sexi 5', true],
-  ['51992621601', 'Botcito Sexi 6', false],
-  ['51992621601', 'Botcito Sexi 7', true],
-  ['51992621601', 'Botcito Sexi 8', true],
-  ['51992621601', 'Botcito Sexi 9', true],
-  ['51992621601', 'Botcito Sexi 19', true], 
-  ['51992621601', 'Botcito Sexi 11', true],
-  ['51992621601', 'Botcito Sexi 12', true], 
-  ['51992621601'],
-  ['51992621601'],
-  ['51992621601'],
-  ['51992621601'],
-  ['51992621601'], 
-  ['51992621601'],  
-  ['51992621601'],
-  ['51992621601'],
-  ['51992621601'],
-  ['51992621601'], 
-  ['51992621601'], 
-  ['51992621601'],
-  ['51992621601'],
-  ['51992621601'],
-  ['51992621601'],
-  ['51992621601'], 
-  ['51992621601'],
-  ['51992621601'],
-  ['51992621601'],
-  ['51992621601']
+  ['56993761200', 'Botcito Sexi', true],
+  ['56993761200', 'Botcito Sexi 1', true],
+  ['56993761200', 'Botcito Sexi 2', true],
+  ['56993761200', 'Botcito Sexi 3', true],
+  ['56993761200', 'Botcito Sexi 4', true],
+  ['56993761200', 'Botcito Sexi 5', true],
+  ['56993761200', 'Botcito Sexi 6', false],
+  ['56993761200', 'Botcito Sexi 7', true],
+  ['56993761200', 'Botcito Sexi 8', true],
+  ['56993761200', 'Botcito Sexi 9', true],
+  ['56993761200', 'Botcito Sexi 19', true], 
+  ['56993761200', 'Botcito Sexi 11', true],
+  ['56993761200', 'Botcito Sexi 12', true], 
+  ['56993761200'],
+  ['56993761200'],
+  ['56993761200'],
+  ['56993761200'],
+  ['56993761200'], 
+  ['56993761200'],  
+  ['56993761200'],
+  ['56993761200'],
+  ['56993761200'],
+  ['56993761200'], 
+  ['56993761200'], 
+  ['56993761200'],
+  ['56993761200'],
+  ['56993761200'],
+  ['56993761200'],
+  ['56993761200'], 
+  ['56993761200'],
+  ['56993761200'],
+  ['56993761200'],
+  ['56993761200']
 ];
 
-global.suittag = ['51992621601'];
-global.prems = ['51992621601'];
+global.suittag = ['56993761200'];
+global.prems = ['56993761200'];
 
 global.packname = 'Sticker';
-global.author = 'Readz Aleizn';
-global.wm = 'Readz Aleizn';
-global.titulowm = 'Readz Aleizn';
-global.titulowm2 = `Readz Aleizn`
-global.igfg = 'Readz Aleizn';
+global.author = '*ElmoBot*';
+global.wm = '*ElmoBot*';
+global.titulowm = '*ElmoBot*';
+global.titulowm2 = `*ElmoBot*`
+global.igfg = '*ElmoBot*';
 global.wait = '*_[ ⏳ ] Cargando..._*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
@@ -79,16 +79,16 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nReadz Aleizn`;
-global.gt = 'Readz Aleizn';
-global.mysticbot = 'Readz Aleizn';
+global.wm2 = `${dia} ${fecha}\n*ElmoBot*`;
+global.gt = '*ElmoBot*';
+global.mysticbot = '*ElmoBot*';
 global.channel = 'https://whatsapp.com/channel/0029Vaein6eInlqIsCXpDs3y';
-global.md = 'https://chat.whatsapp.com/CCDGO1lIuH3Ggr4nA44cbY';
-global.mysticbot = 'https://chat.whatsapp.com/CCDGO1lIuH3Ggr4nA44cbY';
+global.md = 'https://chat.whatsapp.com/LxUojYCbNsr51Okm42spfT';
+global.mysticbot = 'https://chat.whatsapp.com/LxUojYCbNsr51Okm42spfT';
 global.waitt = '*_[ ⏳ ] Cargando..._*';
 global.waittt = '*_[ ⏳ ] Cargando..._*';
 global.waitttt = '*_[ ⏳ ] Cargando..._*';
-global.nomorown = '51992621601';
+global.nomorown = '56993761200';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
